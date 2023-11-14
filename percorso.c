8436bdfcbeb4e74ea7468c808f5c1a3bd2e0af65
Alessandro Gorla <alessandro6.gorla@mail.polimi.it>
2023-11-14 14:31:22 +0100

    Percorso PT 2

---
+++ percorso.c
@@ -0,0 +1,105 @@
+
+/*
+Si vuole realizzare in C un programma "interattivo" che permetta di
+gestire un percorso su uno spazio bidimensionale composto da una
+lista di località.
+Dichiarare un tipo di dato per rappresentare una località su una
+cartina in termini di coordinate x e y (due interi) ed un nome (una
+stringa di al max 30 caratteri). Dichiarare in seguito un tipo di
+dato per rappresentare un massimo di 50 località.
+Scrivere un programma che visualizza un menù con le seguenti
+funzionalità e chiede all'utente di selezionarne una da eseguire:
+1 - Inserire in coda una nuova località
+2 - Inserire in una specificata posizione una nuova località
+3 - Cancellare una località nella lista
+4 - Visualizzare l'intero percorso
+5 - Visualizzare i dati delle località con uno specifico nome
+6 - Visualizzare la lunghezza complessiva dell'itinerario
+7 - Visualizzare la coppia di località (anche non consecutive nel
+percorso) la cui distanza è maggiore di qualsiasi altra coppia.
+8 - Uscita
+Il programma esegue la funzionalità richiesta e una volta completata
+riparte dall'inizio visualizzando il menù e chiedendo una nuova
+scelta. Il programma termina quando l'utente seleziona la
+funzionalità uscita. Se l'utente inserisce un valore sbagliato, il
+programma visualizza un messaggio di errore e ritorna dal menù.
+*/
+#include<stdio.h>
+#include<string.h>
+
+#define MAXS 30
+#define MAXL 50
+
+#define APPENDI 1
+#define VISUALIZZA 4
+#define CERCA 5
+#define USCITA 8
+
+typedef struct {
+  int x, y;
+  char nome[MAXS+1];
+} localita_t;
+
+typedef struct {
+  localita_t loc[MAXL];
+  int n_loc;
+} percorso_t;
+
+void visualizza(percorso_t*);
+void appendi(percorso_t*);
+void cerca(percorso_t*);
+
+int main(){
+  percorso_t p;
+  int scelta;
+
+  p.n_loc = 0;
+
+  do{
+    printf("\nMENU:\n%d - Inserisci in coda\n"
+           "%d - Visualizza percorso\n%d - Cerca località\n%d - Esci\n"
+           "Cosa vuoi fare?\n",
+           APPENDI, VISUALIZZA, CERCA, USCITA);
+    scanf("%d", &scelta);
+    if(scelta == APPENDI){
+      appendi(&p);
+    } else if(scelta == VISUALIZZA){
+      visualizza(&p);
+    } else if(scelta == CERCA){
+      cerca(&p);
+    } else if(scelta != USCITA){
+      printf("Errore nell'inserimento!\n");
+    }
+  } while(scelta != USCITA);
+  printf("\nArrivederci!\n");
+
+  return 0;
+}
+
+void visualizza(percorso_t *p){
+  int i;
+  printf("\nPercorso:\n");
+  for(i=0; i<p->n_loc; i++)
+    printf("%d - %d %d %s\n", i, p->loc[i].x, p->loc[i].y, p->loc[i].nome);
+}
+
+void appendi(percorso_t *p) {
+  if(p->n_loc<MAXL){
+    printf("\nInserire i dati della nuova località:\n");
+    scanf("%d %d %s", &p->loc[p->n_loc].x, &p->loc[p->n_loc].y, p->loc[p->n_loc].nome);
+    p->n_loc++;
+  } else
+    printf("\nSpazio esaurito!\n");
+}
+
+void cerca(percorso_t *p) {
+  char str[MAXS+1];
+  int i;
+  printf("\nInserisci il nome da cercare:\n");
+  scanf("%s", str);
+  printf("Località trovate:\n");
+  for(i=0; i<p->n_loc; i++)
+    if(!strcmp(str, p->loc[i].nome)){
+      printf("%d - %d %d %s\n", i, p->loc[i].x, p->loc[i].y, p->loc[i].nome);
+    }
+}
