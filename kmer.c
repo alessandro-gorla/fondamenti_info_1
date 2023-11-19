3c1763356c14348773e7850cf551aca32f2db314
Alessandro Gorla <alessandro6.gorla@mail.polimi.it>
2023-11-19 16:39:18 +0100

    kmer

---
+++ kmer.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/*Conta quante volte il KMER compare esattamente nelle sequenza SEQ*/
+int conta(char seq[], char kmer[]);
+/**/
+int distanza(char seq1[], char seq2[]);
+/**/
+int contad(char seq[], char kmer[], int d);
+
+int main(){
+    char seq[];
+    char kmer[];
+    int d;
+
+    return 0;
+}
+
+/*Conta quante volte il KMER compare esattamente nelle sequenza SEQ*/
+int conta(char seq[], char kmer[]){
+
+    
+}
+
+/**/
+int distanza(char seq1[], char seq2[]){
+
+}
+
+/**/
+int contad(char seq[], char kmer[], int d){
+
+}
